82f322f16f0b6bb77deb85f82d131ca29382638c
Pikatchu99 <yemalin.agbangla@epitech.eu>
2021-10-01 14:55:03 +0100

    Step 3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -64,6 +64,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:name, :details)
+      params.require(:task).permit(:name, :details, :expired_at)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
     validates :name, presence: true
     validates :details, presence: true
+    validates :expired_at, presence: true
 end
