a585c2d5fe919574ba4254f7771242e2b0a39aef
Pikatchu99 <yemalin.agbangla@epitech.eu>
2021-10-04 16:29:09 +0100

    Step 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  root to: 'users#new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   resources :tasks
   get "/search", to: "tasks#search", as: "search_tasks"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_132553) do
+ActiveRecord::Schema.define(version: 2021_10_04_150812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2021_10_04_132553) do
     t.datetime "expired_at", default: "2021-10-04 07:39:50", null: false
     t.string "status"
     t.integer "priority"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
