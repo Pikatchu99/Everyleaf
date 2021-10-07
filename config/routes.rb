565754ee72c860df5550fe1ec504f03d69e15c5e
Pikatchu99 <yemalin.agbangla@epitech.eu>
2021-10-07 07:04:00 +0100

    Step 5 rewrite

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,5 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
   end
-  get '/404', to: 'errors#not_found'
-  get '/422', to: 'errors#unprocessable'
-  get '/500', to: 'errors#internal_server'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
